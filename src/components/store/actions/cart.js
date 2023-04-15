727d2dd8896d46b15b2f224cb4ee1838587b256b
João Victor <jvmarques0798@gmail.com>
2023-04-14 21:05:12 -0300

    Projeto para app em React de um ecommerce

---
+++ src/components/store/actions/cart.js
@@ -0,0 +1,48 @@
+const Add = (cart, product) => {
+    return{
+        type: 'ADD_TO_CART',
+        cart,
+        product
+    }
+}
+
+const AddItem = (cart, product) => {
+    return{
+        type: 'ADD_ITEM',
+        cart,
+        product
+    }
+}
+
+const RemoveItem = (cart, product) => {
+    return{
+        type: 'REMOVE_ITEM',
+        cart,
+        product
+    }
+}
+
+const DeleteItem = (cart, product) => {
+    return{
+        type: 'DELETE_ITEM',
+        cart,
+        product
+    }
+}
+
+const ChangeCart = (localCart) => {
+    return{
+        type: 'CHANGE_CART',
+        localCart
+    }
+}
+
+const cartObj = {
+    Add,
+    AddItem,
+    RemoveItem,
+    DeleteItem,
+    ChangeCart
+}
+
+export default cartObj
